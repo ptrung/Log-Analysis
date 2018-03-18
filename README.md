e39fae74ee2d304c964e95651b001c586977add9
Pauline Trung <pauline.trung@aon.at>
2018-03-18 09:54:40 +0100

    Add project code

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# Log-Analysis
-Internal reporting tool which takes log data of a database and calculates statistics from it. 
+This project is an internal reporting tool which takes log data of a database and calculates statistics from it. 
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. 
+
+### Install
+
+First of all, you have to install the following programms: 
+1. [Python3](https://www.python.org/downloads)
+2. [VirtualBox](https://www.virtualbox.org/wiki/Downloads)
+3. [Vagrant](https://www.vagrantup.com/downloads.html)
+
+### Setup the project
+1. Download/Clone the [fullstack-nanodegree-vm](https://github.com/udacity/fullstack-nanodegree-vm) repository.
+2. Download and unzip the [database](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip). The file inside ist called *newsdata.sql*.
+3. Download/clone this repository and copy it with the *newsdata.sql* into Vagrant sub-directory in the *fullstack-nanodegree-vm* 
+
+### Launching the Virtual Machine:
+  1. Use your terminal and change the directory to the Vagrant sub-directory and use command `vagrant up` to set up vagrant
+  2. Use `vagrant ssh` to log into Vagrant VM
+
+### Run
+
+1. Laod the data into localdatabase with the command: `psql -d news -f newsdata.sql`
+2. Run the program with: `python log.py`
+
+### Output
+
+If the programm runs correctly, you will the the following output: 
+```
+THE 3 MOST POPULAR ARTICLE:
+1 . Candidate is jerk, alleges rival - 338647 views
+2 . Bears love berries, alleges bear - 253801 views
+3 . Bad things gone, say good people - 170098 views
+
+THE MOST POPULAR AUTHORS:
+1 . Ursula La Multa - 507594 views
+2 . Rudolf von Treppenwitz - 423457 views
+3 . Anonymous Contributor - 170098 views
+4 . Markoff Chaney - 84557 views
+
+DAYS ON WHICH MORE THAN 1% OF REQUESTS LEAD TO ERROR:
+1 . 2016-07-17 - 2.26 %
+```
+
+## Acknowledge
+
+This project was built as part of the Udacity course "Fullstack Web Developer Nanodegree". 
---
+++ log.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python3
+
+import psycopg2
+
+DBNAME = "news"
+
+q1 = """SELECT articles.title, count(*) AS num
+        FROM articles JOIN log
+        ON log.path = '/article/' || articles.slug
+        GROUP BY articles.title
+        ORDER BY num desc
+        LIMIT 3"""
+
+q2 = """SELECT author.name, sum(count.num) AS sum
+        FROM (
+            SELECT authors.name AS name, articles.title AS title
+            FROM authors JOIN articles
+            ON authors.id = articles.author
+        ) AS author
+        JOIN (
+            SELECT articles.title AS title, count(*) AS num
+            FROM articles JOIN log
+            ON log.path = '/article/' || articles.slug
+            GROUP BY articles.title
+            ORDER BY num desc
+        ) AS count
+        ON author.title = count.title
+        GROUP BY author.name
+        ORDER BY sum desc"""
+
+q3 = """SELECT data.date, round(data.percent, 2)
+        FROM (
+            SELECT error.date AS date,
+                (error.count::NUMERIC / ok.count::NUMERIC * 100) AS percent
+            FROM (
+                SELECT date(time) AS date, count(status) AS count
+                FROM log WHERE status = '404 NOT FOUND'
+                GROUP BY date(time)
+                ORDER BY date(time)
+            ) AS error
+            JOIN (
+                SELECT date(time) AS date, count(status) AS count
+                FROM log
+                GROUP BY date(time)
+                ORDER BY date(time)
+            ) AS ok
+            ON error.date = ok.date
+            ORDER BY error.date
+        ) AS data
+        WHERE data.percent > 1
+        ORDER BY data.date desc"""
+
+
+def print_popular_articles():
+    """Prints the 3 most popular articles with their views
+
+    Conntects to the default news database and
+    prints their title with their views.
+    """
+
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+
+    print("\nTHE 3 MOST POPULAR ARTICLE:")
+    c.execute(q1)
+
+    result = c.fetchall()
+
+    count = 1
+    for title, num in result:
+        print(count, ".", title, "-", num, "views")
+        count += 1
+
+    db.close()
+
+
+def print_popular_authors():
+    """Prints the most popular authors with their views
+
+    Connects to the default news database and prints the author names
+    with the summarized views of all their articles
+    """
+
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+
+    print("\nTHE MOST POPULAR AUTHORS:")
+    c.execute(q2)
+
+    result = c.fetchall()
+
+    count = 1
+    for name, sum in result:
+        print(count, ".", name, "-", sum, "views")
+        count += 1
+
+    db.close()
+
+
+def print_request_error():
+    """Prints the days on which more than 1% of the request lead to error
+
+    Connects to the default news database and prints the date of the days
+    on which more than 1% of the requests lead to error. Additionally,
+    the percentage of the errors is displayed.
+    """
+
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+
+    print("\nDAYS ON WHICH MORE THAN 1% OF REQUESTS LEAD TO ERROR:")
+    c.execute(q3)
+
+    result = c.fetchall()
+
+    count = 1
+    for date, percent in result:
+        print(count, ".", date, "-", percent, "%")
+
+    db.close()
+
+
+print_popular_articles()
+print_popular_authors()
+print_request_error()
